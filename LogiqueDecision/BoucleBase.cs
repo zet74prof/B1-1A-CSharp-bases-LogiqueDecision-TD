d3e36f6c87bb038ea216d80553aae955c456358a
Etienne Buffet <Etienne.Buffet@ac-grenoble.fr>
2023-09-25 16:08:45 +0200

    Ajoutez des fichiers projet.

---
+++ LogiqueDecision/BoucleBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiqueDecision
+{
+    static class BoucleBase
+    {
+        public static void Main() {
+            char action = 'O';
+
+            while (action != 'X')
+            {
+                Console.WriteLine("Saisissez X pour quitter:");
+                action = Console.ReadKey(true).KeyChar;
+            }
+
+        }
+
+    }
+}
---
+++ LogiqueDecision/Program.cs
@@ -0,0 +1,34 @@
+﻿//Pour la gestion du score (joueur 1, joueur 2 et score en cours de partie)
+int scoreJoueur1 = 0;
+int scoreJoueur2 = 0;
+int score = 0;
+
+//Pour générer un nombre aléatoire
+Random aleatoire = new Random();
+
+//Pour récupérer le nom du joueur qui doit jouer
+string nomJoueurActif = "";
+
+//Pour jouer à tour de role
+bool joueur1 = true;
+
+//Pour récupérer les valeurs des 2 dés
+int nombreDe1;
+int nombreDe2;
+
+
+//Récupération des noms des joueurs
+Console.Write("Saisissez le nom du joueur 1: ");
+string nomJoueur1 = Console.ReadLine();
+Console.Write("Saisissez le nom du joueur 2: ");
+string nomJoueur2 = Console.ReadLine();
+
+char action = 'O';
+
+while (action != 'X')
+{
+    
+
+}
+
+
